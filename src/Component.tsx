46ca67743261c7e6cb29dd5663270d4bcd60aac1
Serabass <serabass565@gmail.com>
2022-06-14 11:10:29 +0600

    upd

---
+++ src/Component.tsx
@@ -0,0 +1,17 @@
+export function Component() {
+  return (
+    <div>
+      <h1>Component</h1>
+
+      <div
+        style={{
+          backgroundColor: "#f0f0f0",
+          padding: "20px",
+          borderRadius: "5px",
+        }}
+      >
+        body
+      </div>
+    </div>
+  )
+}
