53cb2bc883bfd1cc59e1e0f6b666764d7c4e1a7e
Serabass <serabass565@gmail.com>
2022-06-15 02:52:34 +0600

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css"
 
 function App() {
   return (
-    <div className="App">
+    <   div  className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
